f5c5533c01cf6210ed99c618fd6e7ed17770284f
asom alwafy <alwafyasom@gmail.com>
2022-04-20 05:14:12 +0300

    add authentication controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\login\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,9 @@ Route::get('/', function () {
 });
 
 Route::post('/register',[RegisterController::class,'register']);
+Route::group(['middleware'=>'auth'],function(){
+    
 
+});
+Route::get('/logins',[LoginController::class,'login'])->name('logins'); 
+Route::post('/logins',[LoginController::class,'doLogin'])->name('logins');
