d0a55e4810b63e04ce686dc0d45bb02ebeae0964
dely22 <almktaryh@gmail.com>
2022-04-24 04:39:28 -0800

    add - auth and admin Controllers

---
+++ app/Http/Controllers/Front/adminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class adminController extends Controller
+{
+    // public function admin()
+    // {
+    //     // return view('admin.dashboard');
+    // }
+}
---
+++ resources/views/front/ads.blade.php
@@ -0,0 +1,18 @@
+
+@extends('layouts.masterFront')
+
+    @section('content')
+
+        <!-- ads section start -->
+        <section class="contact-section">
+            <div class="container">
+                <div class="section-title">
+                    <h2 data-text="ads">Get In Touch</h2>
+                </div>
+
+
+            </div>
+        </section>
+        <!-- ads section end -->
+
+    @stop
