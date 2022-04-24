5f9789631f8f132d584c320354514f25341744e9
WafaAlsabri <wafaalsabri23@gmail.com>
2022-04-24 23:14:36 +0300

    add routs of auth front

--- resources/views/auth/admin/admin_login.blade.php
+++ resources/views/auth/admin/admin_login.blade.php
@@ -6,7 +6,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
