34b0bc9f0e4a7a60d8f867a56836ed0fab147eb0
WafaAlsabri <wafaalsabri23@gmail.com>
2022-04-26 14:55:42 +0300

    edit rowts of auth

--- app/Http/Controllers/Front/authController.php
+++ app/Http/Controllers/Front/authController.php
@@ -9,18 +9,18 @@ class authController extends Controller
 {
      public function adminLogin()
    {
-      return view('auth.admin.admin_login');
+      return view('auth.login');
        //  return "login admin Page";
      }
 
      public function userLogin()
      {
-        return view('auth.user.user_login');
+        return view('auth.login');
          
        }
        public function pharmasticLogin()
        {
-          return view('auth.pharmacist.pharmacist_login');
+          return view('auth.login');
           
          }
 
@@ -29,12 +29,12 @@ class authController extends Controller
       
            public function userSignup()
            {
-              return view('auth.user.user_login');
+              return view('auth.register');
                
              }
              public function pharmasticSignup()
              {
-                return view('auth.pharmacist.pharmistic_signup');
+                return view('auth.register');
                 
                }
 
@@ -44,18 +44,18 @@ class authController extends Controller
 
 public function adminForget_password()
 {
-   return view('auth.admin.forget_password');
+   return view('auth.forget-password');
     //  return "login admin Page";
   }
 
   public function userForget_password()
   {
-     return view('auth.user.forget_password');
+     return view('auth.forget-password');
       
     }
     public function pharmasticForget_password()
     {
-       return view('auth.pharmacist.forget_password');
+       return view('auth.forget-password');
        
       }
 
@@ -63,18 +63,18 @@ public function adminForget_password()
 
    public function adminReset_password()
    {
-      return view('auth.admin.resert_password');
+      return view('auth.reset-password');
        //  return "login admin Page";
      }
    
      public function userReset_password()
      {
-        return view('auth.user.resert_password');
+        return view('auth.reset-password');
          
        }
        public function pharmasticReset_password()
        {
-          return view('auth.pharmacist.resert_password');
+          return view('auth.reset-password');
           
          }
    
