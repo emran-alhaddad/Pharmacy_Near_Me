3197912d5fa119f4377d323951d981e94d221e8c
dely22 <almktaryh@gmail.com>
2022-04-28 02:07:56 -0800

    Add - User Profile

---
+++ resources/views/layouts/masterUser.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+    <link  href="{{ asset('auth/css/bootstrap.min.css')}}" rel="stylesheet">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+  <style>
+
+        :root {
+            --black: #444;
+            --light-color: #777;
+            --box-shadow: .5rem .5rem 0 rgba(22, 160, 133, .2);
+            --text-shadow: .4rem .4rem 0 rgba(0, 0, 0, .2);
+            --border: .2rem solid var(--main-color);
+
+            --main-color: #543ab7;
+            --secondary-color: #00acc1;
+            --hover-main: #817ecd;
+            --hover-secondary: #9bb0dd;
+            --bg-sec: #f2f2fa;
+
+            --radius: .5rem;
+        }
+
+        * {
+            font-family: 'Poppins', sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            outline: none;
+            border: none;
+            text-transform: capitalize;
+            transition: all .2s ease-out;
+            text-decoration: none;
+        }
+
+        html {
+            font-size: 95%;
+            overflow-x: hidden;
+            scroll-padding-top: 7rem;
+            scroll-behavior: smooth;
+        }
+
+        html::-webkit-scrollbar {
+            width: 1rem;
+        }
+
+        html::-webkit-scrollbar-track {
+            background: transparent;
+        }
+
+        html::-webkit-scrollbar-thumb {
+            background: linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%);
+        }
+        .btn, .bg-primary  {
+
+            border: var(--border);
+            border-radius: var(--radius);
+            box-shadow: var(--box-shadow);
+            color:  rgba(84, 58, 183, 1);
+            cursor: pointer;
+            text-align: center;
+        }
+        .btn:hover {
+            background: var(--main-color);
+            color: #fff;
+            cursor: pointer;
+        }
+        .color-user{
+        color:  var(--black);
+        padding: .2rem;
+        text-decoration: none;
+        }
+        .color-user:hover{
+        color:  var(--main-color);;
+        }
+        i{
+        color: var(--main-color);
+        }
+
+        .log-btn {
+            display: inline-block;
+            margin-top: 1rem;
+            padding: .7rem;
+            border: var(--border);
+            border-radius: var(--radius);
+            color: var(--main-color);
+            cursor: pointer;
+        }
+
+        .log-btn:hover {
+            /* background-color: var(--hover-main); */
+            cursor: pointer;
+            /* color: #ffffff; */
+        }
+        .s-btn {
+            width: 100%;
+            padding: 1rem 1.2rem;
+            font-size: 1.6rem;
+            font-weight: 800;
+            color: var(--black);
+            border: solid 1px var(--main-color);
+            margin: 1rem 0;
+            text-transform: none;
+            border-radius: var(--radius);
+        }
+        input[type="number"]{
+        width: 49px;
+        border: var(--border);
+        text-align: center;
+        }
+    </style>
+</head>
+<body class="bg-light" style="color: rgb(102, 62, 96);">
+    <main class="container">
+        <!-- top nav start -->
+         @include('includes.UserTopNav')
+        <!-- top nav end -->
+
+        <!-- side Nav Start -->
+         @include('includes.UserSideNav')
+        <!-- side Nav end -->
+
+
+
+            @yield('content')
+
+
+
+     <script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
+     <script src="{{ asset('js/jquery.min.js')}}"></script>
+     <script>
+        // $('personal').click($('pp').hide());
+        $(document).ready(function(){
+
+        $("#perso,#wrk,#skil,#Edu,#expe,#curs,#Edpers").hide();
+
+
+        });
+
+        $("#personal").click(function(){
+        $("#ord").hide();
+        $("#perso").show();
+
+
+
+        });
+        //
+        $("#order").click(function(){
+        $("#perso").hide();
+        $("#ord").show();
+        });
+        //
+        $("#order-repate").click(function(){
+        // $("#repe").hide();
+        //  $("#wrk,#skil,#Edu,#expe,#curs,#Edpers,#perso").hide();
+        $("#rep").show();
+        });
+
+
+
+        $("#Edit").click(function(){
+        $("#wrk,#skil,#Edu,#expe,#curs,#Edpers,#perso").hide();
+        $("#Edpers").show();
+        });
+
+        $("#saveEd").click(function(){
+        $("#wrk,#skil,#Edu,#expe,#curs,#Edpers,#Edpers").hide();
+        $("#perso").show();
+        });
+
+        // $("#detelis").click(function(){
+        // $("#wrk,#perso,#Edu,#skil,#curs,#Edpers").hide();
+        // // $("#deteli").show();
+        // });
+
+
+    </script>
+
+
+</body>
+</html>
---
+++ resources/views/user/userProfile.blade.php
@@ -0,0 +1,394 @@
+      <!-- order Section -->
+        <section class="col-lg-8 col-md-8 col-12" id="ord">
+              <div class="card shadow p-3">
+                <div class="card-header bg-transparent d-flex justify-content-between align-items-center">
+                  <h3 class="fw-bold text-prof">ارسال طلب  </h3>
+                  <a role="button" data-bs-toggle="modal"data-bs-target="#edit-info"
+                        class="  border color-user rounded d-flex justify-content-center align-items-lg-center rounded-circle"
+                        style="bottom: 10%;left: 35%; width: 30px;height: 30px;">
+                   <i class="fas fa-edit"></i>
+                </a>
+
+
+                </div>
+
+                <div class="card-body">
+                    <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">اسم العلاج </h6>
+                      </div>
+                      <div class="col-sm-9 text-secondary">
+                       بلا بلا بلا
+                      </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="input-group mb-3">
+                         <input type="file" class="form-control" id="inputGroupFile02" />
+                      </div>
+                    </div>
+                    <hr>
+
+                    <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">الكمية</h6>
+                      </div>
+                      <div class="col-sm-9 text-secondary">
+                        <input type="number" placeholder="1" >
+                      </div>
+                    </div>
+                    <hr>
+                     <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">أقبل البدائل </h6>
+                      </div>
+                      <div class="col-sm-9 text-secondary">
+                      <input type="checkbox" >
+                      </div>
+                    </div>
+                    <hr>
+                     <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">تكرار الطلبية </h6>
+                      </div>
+                       <a class="col-sm-9" role="button "data-bs-toggle="modal"data-bs-target="#rep">
+                      <div class="col-sm-9 text-secondary">
+                      <input type="checkbox" >
+
+                      </div>  </a>
+                    </div>
+                    <hr>
+                    <div class="row">
+                     <button type="button" class="col-12 p-2 m-2 log-btn ">اضافة</button>
+                    </div>
+
+                  </div>
+              </div>
+              <!-- detelis section -->
+            <div class=" col-12 align-content-center" id="detelis">
+                <div class="card p-3">
+                    <div  class="card-header bg-transparent d-flex justify-content-between align-items-center">
+                        <h3 class="fw-bold text-prof">التفاصيل</h3>
+                        </div>
+
+                        <div class="card-body">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-3">اسم العلاج </div>
+                                <div class="col-3">صورة العلاج </div>
+                                <div class="col-2">الكمية </div>
+                                <div class="col-2">البدائل </div>
+                                <div class="col-2">التكرار  </div>
+
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-3">اسم العلاج </div>
+                                <div class="col-3">صورة العلاج </div>
+                                <div class="col-2">الكمية </div>
+                                <div class="col-2">البدائل </div>
+                                <div class="col-2">التكرار  </div>
+
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <button class="col-12 p-2 m-2 log-btn">ارسال الطلبية </button>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+
+              <!-- info Section -->
+            <section class="col-lg-8 col-md-8 col-12" id="perso">
+              <div class="card shadow p-3">
+                <div class="card-header bg-transparent d-flex justify-content-between align-items-center">
+                  <h3 class="fw-bold text-prof">المعلومات الشخصية </h3>
+                  <a role="button" data-bs-toggle="modal"data-bs-target="#edit-info"
+                        class="  border border-primary rounded d-flex justify-content-center align-items-lg-center rounded-circle"
+                        style="bottom: 10%;left: 35%; width: 30px;height: 30px;">
+                   <i class="fas fa-edit"></i>
+                </a>
+
+
+                </div>
+
+                <div class="card-body">
+                    <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">الاسم الكامل </h6>
+                      </div>
+                      <div class="col-sm-9 text-secondary">
+                       هديل
+                      </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">الايميل </h6>
+                      </div>
+                      <div class="col-sm-9 text-secondary">
+                        hadeel@gmail.com
+                      </div>
+                    </div>
+                    <hr>
+
+                    <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">الهاتف</h6>
+                      </div>
+                      <div class="col-sm-9 text-secondary">
+                        (+697) 777 777 777
+                      </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                      <div class="col-sm-3">
+                        <h6 class="mb-0">العنوان</h6>
+                      </div>
+                      <div class="col-sm-9 text-secondary">
+                        تعز - القاهره - المسبخ
+                      </div>
+                    </div>
+
+
+                  </div>
+              </div>
+            </section>
+
+
+        </div>
+
+    </main>
+
+    <!-- Edit user avatar image model -->
+    <div
+      class="modal fade"
+      id="avatar-edit-model"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content p-3">
+          <!-- model header -->
+          <div class="modal-headerd-flex justify-content-between align-items-center">
+            <h4 class="modal-title fw-bold text-center" id="exampleModalLabel">
+              تعديل صورة البروفايل
+            </h4>
+
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+
+          <!-- model body -->
+          <div class="modal-body">
+            <form class="g-3">
+              <div class="input-group mb-3">
+                <input type="file" class="form-control" id="inputGroupFile02" />
+              </div>
+            </form>
+          </div>
+
+          <!-- model footer -->
+          <div class="modal-footer">
+            <button type="button" class="btn bg-primary text-white">
+              update
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- edit user info -->
+    <div
+          class="modal fade"
+          id="edit-info"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content p-3">
+        <div class="modal-header">
+           <h4 class="modal-title fw-bold text-center col-10" id="exampleModalLabel">
+           تعديل المعلومات الشخصية
+          </h4>
+          <button
+            type="button"
+            class="btn-close col-2"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+
+
+        </div>
+
+        <div class="modal-body">
+          <form class="row g-3">
+            <div class="col-6">
+              <!-- [عمم] name -->
+              <div class="input-group">
+                  <input
+                  type="text"
+                  class="form-control"
+                  placeholder="اسمك الكامل "
+                  aria-label="fName"
+                  aria-describedby="addon-wrapping"
+                  required
+                  value=""
+                  />
+              </div>
+              </div>
+
+
+              <!-- email -->
+              <div class="col-6">
+              <div class="input-group">
+                  <input
+                  type="email"
+                  class="form-control"
+                  placeholder="الايميل"
+                  aria-label="email"
+                  aria-describedby="addon-wrapping"
+                  required
+                  value=""
+                  />
+              </div>
+              </div>
+
+
+              <!-- phone number -->
+              <div class="col-6">
+              <div class="input-group">
+                  <input
+                  type="text"
+                  class="form-contro 1"
+                  placeholder="رقم الهاتف"
+                  aria-label="phone"
+                  aria-describedby="addon-wrapping"
+                  required
+                  value=""
+                  />
+              </div>
+              </div>
+
+              <!-- Address  -->
+              <div class="col-6">
+              <div class="input-group">
+                    <input
+                  type="text"
+                  class="form-control l"
+                  placeholder="العنوان"
+                  aria-label="add"
+                  aria-describedby="addon-wrapping"
+                  required
+                  value=""
+                  />
+              </div>
+              </div>
+
+              <!-- bio -->
+              <div class="col-12">
+              <div class="input-group">
+                    <input
+                  type="text"
+                  class="form-control l"
+                  placeholder="سيرة ذاتية"
+                  aria-label="add"
+                  aria-describedby="addon-wrapping"
+                  required
+                  value=""
+                  />
+              </div>
+              </div>
+
+
+          </form>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class=" col-12 btn bg-primary text-white">Save</button>
+        </div>
+      </div>
+      </div>
+    </div>
+
+  <!-- Repeate modal -->
+    <div  class="modal fade"
+      id="rep"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true" >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body fw-bold d-flex justify-content-lg-center align-items-center flex-column">
+            <h3 class="fs-4">تكرار الطلبية كل </h3>
+            <div class="col">
+              <div class="d-inline-block p-2"> <input type="number" placeholder="0"><h3 class="text-center">يوم</h3></div>
+               <div class="d-inline-block  p-2"> <input type="number" placeholder="0"><h3 class="text-center">شهر</h3></div>
+              <div class="d-inline-block  p-2"> <input type="number" placeholder="0"><h3 class="text-center">أسبوع</h3></div>
+               <div class="d-inline-block  p-2"> <input type="number" placeholder="0"><h3 class="text-center">سنه</h3></div>
+            </div>
+            <div class="col">
+            <h3 class="fs-4"><input type="number" placeholder="0"> لمدة  </h3>
+            <div class="nav-item dropdown col-3  p-2 d-flex justify-content-end">
+                <ul class="dropdown-menu" aria-labelledby="nav">
+                  <li><a class="dropdown-item fs-6 fw-bold " href="#">يوم</a></li>
+                  <li><a class="dropdown-item fs-6 fw-bold" href="#"> أسبوع </a></li>
+                  <li><a class="dropdown-item fs-6 fw-bold " href="#">شهر</a></li>
+                  <li><a class="dropdown-item fs-6 fw-bold" href="#"> سنه </a></li>
+                </ul>
+              </div>
+              </div>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button"
+              class="btn"
+              data-bs-dismiss="modal" >
+              تم
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Delete  modal -->
+    <div  class="modal fade"
+      id="delete"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true" >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div
+            class="modal-body fw-bold d-flex justify-content-lg-center align-items-center flex-column"
+          >
+            <i class="bi bi-exclamation-circle fs-1 text-danger"></i>
+            <h3 class="fs-4">هل تريد الحذف ؟</h3>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-bs-dismiss="modal"
+            >
+              لا
+            </button>
+            <button type="button" class="btn btn-danger">نعم </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
