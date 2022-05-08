465223ab187adae9480bfd9850c62b21ad3c4714
dely22 <almktaryh@gmail.com>
2022-05-08 15:39:15 -0800

    add - pharmacy layout, aside, and nav

---
+++ resources/views/includes/pharmacy/PharmacyAside.blade.php
@@ -0,0 +1,51 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme shadow  my-3 mb-5 rounded-3">
+    <div class="app-brand demo">
+    <a href="index.html" class="app-brand-link">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('Front/assets/images/about/علاجي-01-3.svg') }}" id="logo" alt="Logo">
+        </a>
+        {{-- <span class="app-brand-text demo menu-text fw-bolder ms-2">علاجي</span> --}}
+    </a>
+
+    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+        <i class="bx bx-chevron-left bx-sm align-middle"></i>
+    </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+    <li class="menu-item">
+    <a href="pages-account-settings-account.html" class="menu-link">
+        <div data-i18n="Account">الرسائل</div>
+    </a>
+    </li>
+    <li class="menu-item">
+    <a href="pages-account-settings-account.html" class="menu-link">
+        <div data-i18n="Account">ادارة الطلبات</div>
+    </a>
+    </li>
+    <li class="menu-item">
+    <a href="pages-account-settings-account.html" class="menu-link">
+        <div data-i18n="Account">الاشعارات</div>
+    </a>
+    </li>
+    <li class="menu-item">
+    <a href="pages-account-settings-account.html" class="menu-link">
+        <div data-i18n="Account">الشكاوي</div>
+    </a>
+    </li>
+    <li class="menu-item">
+    <a href="pharmacy-accunt.html" class="menu-link">
+        <div data-i18n="Account">البروفايل</div>
+    </a>
+    </li>
+    <li class="menu-item">
+    <a href="pharmacy-accunt.html" class="menu-link">
+        <div data-i18n="Account">تسجيل خروج</div>
+    </a>
+    </li>
+
+
+    </ul>
+</aside>
--- resources/views/layouts/masterPharmacy.blade.php
+++ resources/views/layouts/masterPharmacy.blade.php
@@ -1,19 +1,124 @@
-@include('includes.PharmacyHeader')
-@if (session('error'))
-    <div class="alert alert-danger" role="alert">
-        {!! session('error') !!}
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
+    <title> Pharmacy- Dashboard</title>
+    <meta name="description" content="" />
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('pharmacy/assets/vendor/fonts/boxicons.css')}}" />
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('pharmacy/assets/vendor/css/core.css" class="template-customizer-core-css')}}" />
+    <link rel="stylesheet" href="{{ asset('pharmacy/assets/vendor/css/theme-default.css" class="template-customizer-theme-css')}}" />
+    <link rel="stylesheet" href="{{ asset('pharmacy/assets/css/demo.css')}}" />
+    <script src="{{ asset('pharmacy/assets/js/config.js')}}"></script>
+
+    {{-- Front Style start --}}
+     <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="{{ asset('Front/assets/images/favicon.png') }}" type="image/png">
+
+    <!--====== Animate CSS ======-->
+    <link rel="stylesheet" href="{{ asset('Front/assets/css/animate.css') }}">
+
+    <!--====== Tiny slider CSS ======-->
+    <link rel="stylesheet" href="{{ asset('Front/assets/css/tiny-slider.css') }}">
+
+    <!--====== glightbox CSS ======-->
+    <link rel="stylesheet" href="{{ asset('Front/assets/css/glightbox.min.css') }}">
+
+    <!--====== Line Icons CSS ======-->
+    <link rel="stylesheet" href="{{ asset('Front/assets/css/LineIcons.2.0.css') }}">
+
+    <!--====== Selectr CSS ======-->
+    <link rel="stylesheet" href="{{ asset('Front/assets/css/selectr.css') }}">
+
+    <!--====== Nouislider CSS ======-->
+    <link rel="stylesheet" href="{{ asset('Front/assets/css/nouislider.css') }}">
+
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="{{ asset('Front/assets/css/bootstrap-5.0.5-alpha.min.css') }}">
+
+    {{-- bootstrap icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <!--====== Style CSS ======-->
+    <link rel="stylesheet" href="{{ asset('Front/assets/css/style.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css"
+        integrity="sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q" crossorigin="anonymous">
+    <style>
+        html {
+            /* direction:rtl; */
+            font-family: "Droid Arabic Kufi", "Droid Sans", sans-serif;
+            font-size: 14px;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        .heading {
+            text-align: center;
+            padding-bottom: 2rem;
+            color: var(--black);
+            font-size: 3.5rem;
+            /* letter-spacing: .4rem; */
+        }
+
+        .heading span {
+            color: var(--main-color);
+        }
+
+        .btn-submit {
+            border: var(--border);
+            border-radius: var(--radius);
+            color: #ffffff;
+            background-color: var(--main-color);
+        }
+
+        .btn-submit :hover {
+            color: var(--main-color);
+            background-color: #ffffff;
+        }
+
+    </style>
+    {{-- Front Style end --}}
+  </head>
+
+  <body style="direction: rtl;">
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+                @include('includes.pharmacy.PharmacyAside')
+            <!-- / Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+                    @include('includes.pharmacy.PharmacyNav')
+                <!-- / Navbar -->
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                     @yield('content')
+
+
+                </div>
+            </div>
+        </div>
     </div>
-@endif
-@if (session('status'))
-    <div class="alert alert-success" role="alert">
-        {!! session('status') !!}
-    </div>
-@endif
-@yield('phar_profile_content')
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-</body>
+    <!-- Core JS -->
+    <script src="{{ asset('pharmacy/assets/vendor/js/bootstrap.js')}}"></script>
+    <script src="{{ asset('pharmacy/assets/vendor/js/menu.js')}}"></script>
+    <!-- Main JS -->
+    <script src="{{ asset('pharmacy/assets/js/main.js')}}"></script>
 
+    <!-- Page JS -->
+    <script src="{{ asset('pharmacy/assets/js/pages-account-settings-account.js')}}"></script>
+
+  </body>
 </html>
