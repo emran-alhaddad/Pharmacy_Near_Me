686f333b8805f6b260eee8b587d7c60b6594f249
WafaAlsabri <wafaalsabri23@gmail.com>
2022-05-21 01:17:16 +0300

    edit chat

--- resources/views/livewire/message.blade.php
+++ resources/views/livewire/message.blade.php
@@ -1,3 +1,43 @@
+
+
+<?php
+
+if (!function_exists('isPhoto')) 
+{
+    function isPhoto($path) 
+    {
+        // Get the file extension from the path
+        $exploded = explode('.', $path);
+        $ext = strtolower(end($exploded));
+        // Define the photos extensions
+        $photoExtensions = ['png', 'jpg', 'jpeg', 'gif', 'jfif', 'tif', 'webp'];
+        // Check if this extension belongs to the extensions we defined
+        if (in_array($ext, $photoExtensions)) {
+            return true;
+        }
+        return false;
+    }
+}
+
+if (!function_exists('isVideo')) 
+{
+    function isVideo($path) 
+    {
+        // Get the file extension from the path
+        $exploded = explode('.', $path);
+        $ext = end($exploded);
+        // Define the videos extensions
+        $videoExtensions = ['mov', 'mp4', 'avi', 'wmf', 'flv', 'webm'];
+        // Check if this extension belongs to the extensions we defined
+        if (in_array($ext, $videoExtensions)) {
+            return true;
+        }
+        return false;
+    }
+}
+
+?>
+
 <div>
     <div class="row justify-content-center" wire:poll="mountComponent()">
         @if(auth()->user()->is_active == true)
