28376e59aecc557969a0f42745013f6227be7cd0
WafaAlsabri <wafaalsabri23@gmail.com>
2022-05-24 11:12:51 +0300

    chaty2

--- resources/views/user/chat/home.blade.php
+++
@@ -1,8 +0,0 @@
-
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    @livewire('message', ['users' => $users, 'messages' => $messages ?? null])
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\User;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\Pharmacy;
-use App\Http\Controllers\ChatController;
-
 use App\Http\Controllers\Front;
 use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\Auth\Register;
@@ -319,10 +317,6 @@ Route::get('/checkout-order/test/cancel/{cancel}', [Payment\PaymentController::c
 Route::get('/checkout-order/test/cancel', [Payment\PaymentController::class, 'viewCancel'])->name('viewCancel');
 
 Route::get('send', [Notify\NotificationsController::class, 'registerNotification'])->name('viewCancel');
-   //chat routs
-   Route::group(['middleware' => 'auth'], function () {
-    Route::get('/inbox', [ChatController::class, 'index'])->name('inbox.index');
-    Route::get('/inbox/{id}', [ChatController::class, 'show'])->name('inbox.show');
-});
+
 
 
